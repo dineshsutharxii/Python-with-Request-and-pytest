7d3639bdf7179c5efbb3f1f7baada9d665297fb7
dinesh <dineshsutharxii@gmail.com>
2024-08-19 19:22:24 +0530

    Added reqre

---
+++ UsingPytest/test_reqres.py
@@ -0,0 +1,12 @@
+import requests
+
+
+class TestReqres:
+    def test_create_user(self):
+        headers = {
+
+                  }
+        pay_load = {
+                        "name": "morpheus",
+                        "job": "leader"
+                    }
